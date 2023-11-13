622782d1968c107fcd0ae97859e78b191bd4d304
김상우 <electronyoon@gmail.com>
2023-11-13 12:29:29 +0900

    implicit with first-fit implemented

--- mm.c
+++ mm.c
@@ -18,93 +18,248 @@
 #include "mm.h"
 #include "memlib.h"
 
+#define DEBUG
+#ifdef DEBUG
+# define dbg_printblock(a) printblock(a)
+#else
+# define dbg_printblock(a)
+#endif
+
 /*********************************************************
  * NOTE TO STUDENTS: Before you do anything else, please
  * provide your team information in the following struct.
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "1team",
     /* First member's full name */
-    "Harry Bovik",
+    "Sangwoo Kim",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "electronyoon@gmail.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
     ""
 };
 
-/* single word (4) or double word (8) alignment */
+#define WSIZE 4
+#define DSIZE 8
+#define CHUNKSIZE (1<<12)
+#define MAX(x, y) ((x) > (y)? (x) : (y))
+
+#define PACK(size, alloc) ((size) | (alloc))
+
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int*)(p) = (val))
+
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
 #define ALIGNMENT 8
-
-/* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
-
-
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-/* 
- * mm_init - initialize the malloc package.
- */
-int mm_init(void)
-{
-    return 0;
-}
 
-/* 
- * mm_malloc - Allocate a block by incrementing the brk pointer.
- *     Always allocate a block whose size is a multiple of the alignment.
- */
-void *mm_malloc(size_t size)
-{
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+static char *heap_listp;
+
+inline int mm_init(void);
+inline static void *extend_heap(size_t words);
+inline void mm_free(void *bp);
+inline static void *coalesce(void *bp);
+inline void *mm_malloc(size_t size);
+inline static void *find_fit_first(size_t asize);
+inline static void *find_fit_next(size_t asize);
+inline static void place(char *bp, size_t asize);
+
+void print_binary(unsigned int number) {
+    if (number) {
+        print_binary(number >> 1);
+        putc((number & 1) ? '1' : '0', stdout);
     }
 }
 
-/*
- * mm_free - Freeing a block does nothing.
- */
-void mm_free(void *ptr)
-{
+int mm_init(void) {
+    if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+        return -1;
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1));
+    heap_listp += (2*WSIZE);
+
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+        return -1;
+    return 0;
 }
 
-/*
- * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
- */
-void *mm_realloc(void *ptr, size_t size)
+static void *extend_heap(size_t words) {
+    char *bp;
+    size_t size;
+    
+    size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+    
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+    return coalesce(bp);
+}
+
+void mm_free(void *bp) {
+    size_t size = GET_SIZE(HDRP(bp));
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    coalesce(bp);
+}
+
+static void *coalesce(void *bp) {
+    printf("coalescing (%p)... ", bp);
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if (prev_alloc && next_alloc) {
+        printf("no coalescing needed.\n");
+        return bp;
+    } else if (prev_alloc && !next_alloc) {
+        printf("original size: %d\n", size);
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        printf("changed size: %d\n", size);
+        PUT(HDRP(bp), PACK(size, 0));
+        printf("new block header: %p\n", HDRP(bp));
+        PUT(FTRP(bp), PACK(size, 0));
+        printf("new block footer: %p\n", FTRP(bp));
+        printf("next block. start: %p, end: %p\n", bp, NEXT_BLKP(bp));
+    } else if (!prev_alloc && next_alloc) {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+        printf("prev block. start: %p, end: %p\n", bp, NEXT_BLKP(bp));
+    } else {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
+}
+
+void *mm_malloc(size_t size) {
+    size_t asize;
+    size_t extendsize;
+    char *bp;
+    
+    if (size == 0)
+        return NULL;
+
+    if (size <= DSIZE)
+        asize = 2*DSIZE;
+    else
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+
+    if ((bp = find_fit_first(asize)) != NULL) {
+        place(bp, asize);
+        return bp;
+    }
+
+    extendsize = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
+        return NULL;
+    place(bp, asize);
+    return bp;
+}
+
+static void *find_fit_first(size_t asize) {
+    void *bp = heap_listp;
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+            return bp;
+        }
+    }
+    return (void *)0;
+}
+
+static void place(char *bp, size_t asize) {
+    size_t csize = GET_SIZE(HDRP(bp));
+
+    if ((csize - asize) >= (2*DSIZE)) {
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        PUT(HDRP(NEXT_BLKP(bp)), PACK(csize-asize, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(csize-asize, 0));
+    } else {
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
+}
+
+void *mm_realloc(void *bp, size_t size)
 {
-    void *oldptr = ptr;
-    void *newptr;
+    void *old_bp = bp;
+    void *new_bp;
     size_t copySize;
     
-    newptr = mm_malloc(size);
-    if (newptr == NULL)
-      return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+    new_bp = mm_malloc(size);
+    if (new_bp == NULL)
+        return NULL;
+    copySize = GET_SIZE(HDRP(old_bp));
     if (size < copySize)
-      copySize = size;
-    memcpy(newptr, oldptr, copySize);
-    mm_free(oldptr);
-    return newptr;
+        copySize = size;
+    memcpy(new_bp, old_bp, copySize);
+    mm_free(old_bp);
+    return new_bp;
 }
 
 
+int main() {
+    mem_init();
+    mm_init();
 
+    printf("heap start: %p\n", mem_heap_lo());
+    printf("prologue header: %p\n", heap_listp - WSIZE);
+    printf("prologue footer: %p\n", heap_listp);
+    printf("epilogue: %p\n", mem_heap_hi() - WSIZE);
+    printf("heap size: %d\n", mem_heapsize());
+    // printf("sbrk(0): %p\n", mem_sbrk(0));
+    printf("\n");
+    
+    void *bp = mm_malloc(8);
+    printf("bp header: %p\n", HDRP(bp));
+    printf("bp: %p\n", bp);
+    printf("bp footer: %p\n", FTRP(bp));
+    printf("\n");
 
+    void *bp2 = mm_malloc(16);
+    printf("bp2 header: %p\n", HDRP(bp2));
+    printf("bp2: %p\n", bp2);
+    printf("bp2 footer: %p\n", FTRP(bp2));
+    printf("\n");
 
+    // void *bp3 = mm_malloc(8);
+    // printf("bp3 header: %p\n", HDRP(bp3));
+    // printf("bp3: %p\n", bp3);
+    // printf("bp3 footer: %p\n", FTRP(bp3));
+    // printf("\n");
+    
+    mm_free(bp2);
 
+    void *bp4 = mm_malloc(4040);
+    printf("bp4 header: %p\n", HDRP(bp4));
+    printf("bp4: %p\n", bp4);
+    printf("bp4 footer: %p\n", FTRP(bp4));
+    printf("\n");
 
+    printf("heap start: %p\n", mem_heap_lo());
+    printf("changed? epilogue: %p\n", mem_heap_hi() - WSIZE);
+    printf("changed? heap size: %d\n", mem_heapsize());
 
-
-
-
-
-
-
+}
